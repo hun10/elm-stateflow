552016c376a32faf195c7f3695ab1aae37db31a9
Dmitri Urbanowicz <hun10@yandex.ru>
2016-02-17 00:16:48 +0400

    3

--- Flow.elm
+++ Flow.elm
@@ -4,11 +4,15 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 main =
+  view 0.5
+
+view opacity =
   div [ style [ ("padding", "20px")
               , ("margin", "40px")
               , ("border", "1px solid")
               , ("font-family", "Modern")
               , ("font-size", "14pt")
+              , ("opacity", toString opacity)
               ]]
   [ text "Hello helloh helloh helo русские буквы как смотрят"
   ]
