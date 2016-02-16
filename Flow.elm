977a1db4201961546a2589c2fdbaf4b9805de3eb
Dmitri Urbanowicz <hun10@yandex.ru>
2016-02-17 00:13:12 +0400

    2

--- Flow.elm
+++ Flow.elm
@@ -1,6 +1,14 @@
 module Flow where
 
-import Graphics.Element exposing (show)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 main =
-  show "Flow"
+  div [ style [ ("padding", "20px")
+              , ("margin", "40px")
+              , ("border", "1px solid")
+              , ("font-family", "Modern")
+              , ("font-size", "14pt")
+              ]]
+  [ text "Hello helloh helloh helo русские буквы как смотрят"
+  ]
