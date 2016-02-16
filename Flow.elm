b9c7c2eeceb09278a98e6dcd414718db06402780
Dmitri Urbanowicz <hun10@yandex.ru>
2016-02-17 00:30:28 +0400

    6

--- Flow.elm
+++ Flow.elm
@@ -1,10 +1,12 @@
 module Flow where
 
+import Char
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Keyboard
+import String
 
-main = Signal.map (text << toString) Keyboard.presses
+main = Signal.map (text << String.fromChar << Char.fromCode) Keyboard.presses
 --  view 0.5 "Hello helloh helloh helo русские буквы как смотрят"
 
 view opacity txt =
