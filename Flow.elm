f07210069ca9ae5ccabf023dac7e0c6ffe1f0f30
Dmitri Urbanowicz <hun10@yandex.ru>
2016-02-18 15:38:01 +0400

    13

--- Flow.elm
+++ Flow.elm
@@ -5,47 +5,73 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Keyboard
 import String
+import Time exposing (Time)
 
 main =
-  Signal.map (view 0.5) paragraphs
+  Signal.map view state
 
-view opacity paragraphs =
+defaultTimeToFade = 5 * Time.second
+
+view {model, timeToFade} =
   div [ style [ ("padding", "20px")
               , ("font-family", "Modern")
               , ("font-size", "14pt")
-              , ("opacity", toString opacity)
+              , ("opacity", toString <| timeToFade / defaultTimeToFade )
               ]]
-  <| List.map (\txt -> p [] [text txt]) paragraphs
+  <| List.map (\txt -> p [] [text txt]) <| model
 
-paragraphs =
-  Signal.foldp update [] actions
+state =
+  Signal.foldp update { model = [], timeToFade = 0 } actions
 
-update action model =
+update action m =
   let
     reversed = List.reverse model
+    model = m.model
+    timeToFade = m.timeToFade
   in
   case (action, reversed) of
     (Typed s, []) ->
-      [s]
+      { m | model = [s]
+          , timeToFade = defaultTimeToFade
+      }
     
     (NewParagraph, []) ->
-      []
+      { m | model = []
+          , timeToFade = defaultTimeToFade
+      }
     
     (Typed s, last :: previous) ->
-      List.reverse <| (last ++ s) :: previous
+      { m | model = List.reverse <| (last ++ s) :: previous
+          , timeToFade = defaultTimeToFade
+      }
     
     (NewParagraph, "" :: previous) ->
-      model
+      m
     
     (NewParagraph, _) ->
-      model ++ [""]
+      { m | model = model ++ [""]
+          , timeToFade = defaultTimeToFade
+      }
+    
+    (TimeElapsed t, _) ->
+      if t > timeToFade then
+        { model = []
+        , timeToFade = 0
+        }
+      else
+        { m | timeToFade = timeToFade - t
+        }
 
 type Action
   = Typed String
   | NewParagraph
+  | TimeElapsed Time
 
 actions =
-  Signal.mergeMany [newParagraphs, spaceBars, chars]
+  Signal.mergeMany [newParagraphs, spaceBars, chars, ticks]
+
+ticks =
+  Signal.map TimeElapsed (Time.fps 5)
 
 spaceBars =
   Signal.map (\_ -> Typed " ") Keyboard.space
